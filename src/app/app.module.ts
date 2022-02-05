0465164c38a2d321b014a203bcaa4223a0ef568e
Ram Infosystems Limited <rlodhiya2502@gmail.com>
2022-02-05 20:04:22 +0000

    Generate Basic Layout and Framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FwModule } from '../fw/fw.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FwModule
   ],
   providers: [],
   bootstrap: [AppComponent]
